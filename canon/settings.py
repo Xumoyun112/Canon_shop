36c77cf1ba3556daeae28e7c68e029310f9a2bee
avln1104 <avln1104@gmail.com>
2025-10-08 00:30:36 +0500

    add jazzmin

--- canon/settings.py
+++ canon/settings.py
@@ -30,14 +30,14 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["45.55.211.182", "localhost", "domain.com","127.0.0.1", "127.0.0.1:8000"]
 
 # Application definition
 
 INSTALLED_APPS = [
-    # 'jazzmin',
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
